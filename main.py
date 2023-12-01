1914cf3b7234269eb1176a9d0d982d0deed7d099
PabloAMC <pmorenocf@alumnos.unex.es>
2023-12-01 13:40:59 +0100

    Main result code

--- main.py
+++ main.py
@@ -4,21 +4,38 @@ from matplotlib import lines
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import os
 
 from magnus_errors import *
 
+# Get current directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+save_path = os.path.join(dir_path, 'coefficients')
+
 # First, we import the Magnus expansion coefficients
-with open('cs.json', 'r') as f:
-    cs = json.load(f, object_hook=convert_keys_to_float)
+with open(os.path.join(save_path, 'xs.json'), 'r') as f:
+    xs = json.load(f, object_hook=convert_keys_to_float)
 
-with open('cs_y.json', 'r') as f:
-    cs_y = json.load(f, object_hook=convert_keys_to_float)
+with open(os.path.join(save_path,'ys.json'), 'r') as f:
+    ys = json.load(f, object_hook=convert_keys_to_float)
 
-with open('cs_split.json', 'r') as f:
-    cs_split = json.load(f, object_hook=convert_keys_to_float)
+with open(os.path.join(save_path,'zs.json'), 'r') as f:
+    zs = json.load(f, object_hook=convert_keys_to_float)
 
-with open('cs_y_split.json', 'r') as f:
-    cs_y_split = json.load(f, object_hook=convert_keys_to_float)
+with open(os.path.join(save_path,'overline_xs.json'), 'r') as f:
+    overline_xs = json.load(f, object_hook=convert_keys_to_float)
+
+with open(os.path.join(save_path,'xs_split.json'), 'r') as f:
+    xs_split = json.load(f, object_hook=convert_keys_to_float)
+
+with open(os.path.join(save_path, 'ys_split.json'), 'r') as f:
+    ys_split = json.load(f, object_hook=convert_keys_to_float)
+
+with open(os.path.join(save_path, 'zs_split.json'), 'r') as f:
+    zs_split = json.load(f, object_hook=convert_keys_to_float)
+
+with open(os.path.join(save_path, 'overline_xs_split.json'), 'r') as f:
+    overline_xs_split = json.load(f, object_hook=convert_keys_to_float)
 
 def convert_sci_to_readable(number_sci):
     # Convert to readable format
@@ -37,72 +54,57 @@ factorial = {}
 for i in range(0, 75):
     factorial[i] = np.longdouble(math.factorial(i))
 
-hs = [1/2**(i/3+3) for i in range(1,200)]
+hs = [1/2**(i/3+3) for i in range(1,100)]
 total_error_list = [1e-3, 1e-7, 1e-11, 1e-15]
 total_time_list = [2**i for i in range(3, 15)]
 
 ########### Commutator Free Magnus ###########
+range_s = [1, 2, 2, 3, 3, 4]
+range_m = [1, 2, 3, 5, 6, 11]
 
-def error_sum_CF(h, s, m, cs, cs_y, maxc = 1, maxp = 40, use_max = True, n = None):
+def error_sum_CF_wout_trotter(h, s, m, overline_xs, ys, maxc = 1, maxp = 40, use_max = True):
     r"""
     h: step size
     n: number of spins
     s: 2s is the order of the Magnus expansion
     m: number of exponentials in the Commutator Free Magnus operator
-    cs: list of the coefficients of the Magnus expansion in the basis of the Lie algebra
-    cs_y: list of the coefficients of the Magnus expansion in the basis of univariate integrals
+    overline_xs: list of the coefficients of the Magnus expansion in the basis of the Lie algebra
+    ys: list of the coefficients of the Magnus expansion in the basis of univariate integrals
     maxc: maximum value of the norm of the Hamiltonian and its derivatives
     maxp: maximum order of the Magnus expansion
     use_max: if True, the maximum value of the norm of the Hamiltonian and its derivatives is used
     """
     error = np.longdouble(0)
 
-    # First, we add the error from the Taylor truncation of Omega
+    # Error from the Taylor expansion of the exponential of the Magnus expansion
+
     p = 2*s+1
-    omega_error = Omega_bound(h, p, s, maxc)
-    last_correction = omega_error
-    while last_correction/omega_error > 1e-5:
+    exp_omega_error = exp_Omega_bound(h, p, s, maxc, factorial)
+    last_correction = exp_omega_error
+    while last_correction/exp_omega_error > 1e-5:
         p += 1
         if p > maxp:
             raise ValueError('The error is not converging')
-        last_correction = Omega_bound(h, p, s, maxc)
-        omega_error += last_correction
-    error += omega_error
-
+        last_correction = exp_Omega_bound(h, p, s, maxc, factorial)
+        exp_omega_error += last_correction
+    error += exp_omega_error
 
     if s>1:
-    # Error from the Taylor expansion of the exponential of the Magnus expansion
-
-        p = 2*s+1
-        exp_omega_error = exp_Omega_bound(h, p, s, maxc, factorial)
-        last_correction = exp_omega_error
-        while last_correction/exp_omega_error > 1e-5: #todo: change 1e-5
-            p += 1
-            if p > maxp:
-                raise ValueError('The error is not converging')
-            last_correction = exp_Omega_bound(h, p, s, maxc, factorial)
-            exp_omega_error += last_correction
-        error += exp_omega_error
-
 
         # Error from the Taylor expansion of the product of exponentials in the commutator-free operator
-        error += Psi_m_Taylor_error(h, maxp, s, m, cs[s][m], factorial, use_max = use_max or s > 4)
+        error += Psi_m_Taylor_error(h, maxp, s, m, overline_xs[s][m] * maxc, factorial, use_max = use_max or s > 4)
 
         # Error from the quadrature rule
         qr = quadrature_residual(h, s, m, maxc = maxc)
-        error += quadrature_error(h, s, m, cs_y, maxc = maxc, qr = qr)
-
-
-        # Error from the basis change
-        error += basis_change_error(h, s, m, cs_y, maxc)
+        error += quadrature_error(h, s, m, ys, maxc = maxc, qr = qr)
 
 
     # Error from the Trotter product formula
-    error += trotter_error_spu_formula(n, h, s, u = maxc)
+    #error += trotter_error_spu_formula(n, h, s, u = maxc)
     
     return error
 
-def compute_step_error(hs, range_s, range_m, maxp, total_error_list, total_time_list, use_max = True, cs = cs, cs_y = cs_y):
+def compute_step_error_cf(hs, range_s, range_m, maxp, total_time_list, use_max = True, overline_xs = overline_xs, ys = ys):
     r"""
     Computes a dictionary of errors for different values of the step size, the order of the Magnus expansion, and the number of exponentials in the Commutator Free Magnus operator.
 
@@ -117,20 +119,28 @@ def compute_step_error(hs, range_s, range_m, maxp, total_error_list, total_time_
     use_max: if True, the maximum value of the norm of the Hamiltonian and its derivatives is used
     """
     step_error = {}
-    for total_error in tqdm(total_error_list, desc='total_error'):
-        step_error[total_error] = {}
-        for n in tqdm(total_time_list, desc = 'time'):  
-            step_error[total_error][n] = {}
-            for (s, m) in tqdm(zip(range_s, range_m), desc = 's, m'):
-                if s not in step_error[total_error][n].keys():
-                    step_error[total_error][n][s] = {}
-                step_error[total_error][n][s][m] = {}
-                for h in hs:
-                    step_error[total_error][n][s][m][h] = float(error_sum_CF(h, s, m, cs, cs_y, maxc = 1, maxp = maxp, use_max = use_max, n = n))
+    step_error_wout_trotter = {}
+    for (s, m) in tqdm(zip(range_s, range_m), desc = 's, m'):
+        if s not in step_error_wout_trotter.keys():
+            step_error_wout_trotter[s] = {}
+        step_error_wout_trotter[s][m] = {}
+        for h in hs:
+            step_error_wout_trotter[s][m][h] = float(error_sum_CF_wout_trotter(h, s, m, overline_xs, ys, maxc = 1, maxp = maxp, use_max = use_max))
+    for n in tqdm(total_time_list, desc = 'time'):  
+        step_error[n] = {}
+        for (s, m) in tqdm(zip(range_s, range_m), desc = 's, m'):
+            if s not in step_error[n].keys():
+                step_error[n][s] = {}
+            step_error[n][s][m] = {}
+            for h in hs:
+                step_error[n][s][m][h] = float(step_error_wout_trotter[s][m][h] + trotter_error_spu_formula(n, h, s, u = 1))
     return step_error
 
 # We first compute the error of a single step
-#step_error = compute_step_error(hs, range_s, range_m, maxp = 50, total_error_list = total_error_list, total_time_list = total_time_list, use_max = True)
+#step_error_cf = compute_step_error_cf(hs, range_s, range_m, maxp = 50, total_time_list=total_time_list, use_max = True)
+
+#with open('results/step_error_CFMagnus.json', 'w') as f:
+#    json.dump(step_error_cf, f)
 
 with open('results/step_error_CFMagnus.json', 'r') as f:
     step_error_cf = json.load(f, object_hook=convert_keys_to_float)
@@ -158,7 +168,7 @@ def minimize_cost_CFMagnus(hs, s, m, total_time, total_error, step_error, trotte
             cost_exponentials[h] *= 5**(s-1)
         #if split_operator and m == 3 and s== 2: 
         #    cost_exponentials[h] = 5**(s-1) * total_time/h # We can concatenate the first and last exponentials
-        errors[h] = total_time*step_error[total_error][total_time][s][m][h]/h
+        errors[h] = total_time*step_error[total_time][s][m][h]/h
 
     min_cost = np.inf
     for h in hs:
@@ -186,13 +196,13 @@ def error_sum_trotter(h, s, maxc = 1, maxp = 40, n = None):
     # First, we add the error from the Taylor truncation of Omega
     s0 = 1
     p = 2*s0+1 # We are truncating the Magnus expansion at order 2s+1
-    omega_error = Omega_bound(h, p, s, maxc)
+    omega_error = exp_Omega_bound(h, p, s, maxc, factorial)
     last_correction = omega_error
     while last_correction/omega_error > 1e-5:
         p += 1
         if p > maxp:
             raise ValueError('The error is not converging')
-        last_correction = Omega_bound(h, p, s, maxc)
+        last_correction = exp_Omega_bound(h, p, s, maxc, factorial)
         omega_error += last_correction
     error += omega_error
 
@@ -201,7 +211,7 @@ def error_sum_trotter(h, s, maxc = 1, maxp = 40, n = None):
 
     return error
 
-def compute_trotter_step_error(hs, range_s, maxp, total_error_list, total_time_list, use_max = True):
+def compute_trotter_step_error(hs, range_s, maxp, total_time_list, use_max = True):
     r"""
     Computes a dictionary of errors for different values of the step size, the order of the Magnus expansion, and the number of exponentials in the Commutator Free Magnus operator.
 
@@ -216,19 +226,17 @@ def compute_trotter_step_error(hs, range_s, maxp, total_error_list, total_time_l
     use_max: if True, the maximum value of the norm of the Hamiltonian and its derivatives is used
     """
     trotter_error = {}
-    for total_error in tqdm(total_error_list, desc='total_error'):
-        trotter_error[total_error] = {}
-        for n in tqdm(total_time_list, desc = 'time'):  
-            trotter_error[total_error][n] = {}
-            for s in tqdm(range_s, desc = 's'):
-                if s not in trotter_error[total_error][n].keys():
-                    trotter_error[total_error][n][s] = {}
-                for h in hs:
-                    trotter_error[total_error][n][s][h] = float(error_sum_trotter(h, s, maxc = 1, maxp = maxp, n = n))
+    for n in tqdm(total_time_list, desc = 'time'):  
+        trotter_error[n] = {}
+        for s in tqdm(range_s, desc = 's'):
+            if s not in trotter_error[n].keys():
+                trotter_error[n][s] = {}
+            for h in hs:
+                trotter_error[n][s][h] = float(error_sum_trotter(h, s, maxc = 1, maxp = maxp, n = n))
     return trotter_error
 
-#range_s = [1,2,3,4]
-#step_error_trotter = compute_trotter_step_error(hs, range_s, maxp = 50, total_error_list = total_error_list, total_time_list = total_time_list, use_max = True)
+range_s_trotter = [1,2,3,4]
+#step_error_trotter = compute_trotter_step_error(hs, range_s_trotter, maxp = 50, total_time_list = total_time_list, use_max = True)
 
 # json save step_error
 #with open('results/step_error_trotter.json', 'w') as f:
@@ -271,7 +279,7 @@ def minimize_cost_trotter(hs, s, total_time, total_error, step_error, trotter_ex
 
 ########### Commutation-free Magnus split-operator ###########
 
-def error_sum_CFsplit(h, s, m, cs_split, cs_y_split, maxc = 1, maxp = 40, use_max = True, n = None):
+def error_sum_CFsplit(h, s, m, overline_xs_split, ys_split, maxc = 1, maxp = 40, use_max = True):
     r"""
     Computes the step error for a split-operator commutator-free Magnus expansion.
 
@@ -291,17 +299,6 @@ def error_sum_CFsplit(h, s, m, cs_split, cs_y_split, maxc = 1, maxp = 40, use_ma
 
     # First, we add the error from the Taylor truncation of Omega
     p = 2*s+1
-    omega_error = Omega_bound(h, p, s, maxc)
-    last_correction = omega_error
-    while last_correction/omega_error > 1e-5:
-        p += 1
-        if p > maxp:
-            raise ValueError('The error is not converging')
-        last_correction = Omega_bound(h, p, s, maxc)
-        omega_error += last_correction
-    error += omega_error
-
-    p = 2*s+1
     exp_omega_error = exp_Omega_bound(h, p, s, maxc, factorial)
     last_correction = exp_omega_error
     while last_correction/exp_omega_error > 1e-5:
@@ -312,20 +309,16 @@ def error_sum_CFsplit(h, s, m, cs_split, cs_y_split, maxc = 1, maxp = 40, use_ma
         exp_omega_error += last_correction
     error += exp_omega_error
 
-
     # Error from the Taylor expansion of the product of exponentials in the commutator-free operator
-    error += Psi_m_Taylor_error(h, maxp, s, m, cs_split[s][m], factorial, use_max = use_max or s > 4)
+    error += Psi_m_Taylor_error(h, maxp, s, m, overline_xs_split[s][m], factorial, use_max = use_max or s > 4)
 
     # Error from the quadrature rule
     qr = quadrature_residual(h, s, m, maxc = maxc)
-    error += quadrature_error(h, s, m, cs_y_split, maxc = maxc, qr = qr)
-
-    # Error from the basis change
-    error += basis_change_error(h, s, m, cs_y_split, maxc)
+    error += quadrature_error(h, s, m, ys_split, maxc = maxc, qr = qr)
     
     return error
 
-def compute_step_error_split(hs, range_s, range_m, maxp, total_error_list, total_time_list, use_max = True, cs_split = cs_split, cs_y_split = cs_y_split):
+def compute_step_error_split(hs, range_s, range_m, maxp, use_max = True, overline_xs_split = overline_xs_split, ys_split = ys_split):
     r"""
     Computes a dictionary of errors for different values of the step size, the order of the Magnus expansion, and the number of exponentials in the Commutator Free Magnus operator.
 
@@ -340,22 +333,18 @@ def compute_step_error_split(hs, range_s, range_m, maxp, total_error_list, total
     use_max: if True, the maximum value of the norm of the Hamiltonian and its derivatives is used
     """
     step_error = {}
-    for total_error in tqdm(total_error_list, desc='total_error'):
-        step_error[total_error] = {}
-        for n in tqdm(total_time_list, desc = 'time'):  
-            step_error[total_error][n] = {}
-            for (s, m) in tqdm(zip(range_s, range_m), desc = 's, m'):
-                if s not in step_error[total_error][n].keys():
-                    step_error[total_error][n][s] = {}
-                step_error[total_error][n][s][m] = {}
-                for h in hs:
-                    step_error[total_error][n][s][m][h] = float(error_sum_CF(h, s, m, cs_split, cs_y_split, maxc = 1, maxp = maxp, use_max = use_max, n = n))
+    for (s, m) in tqdm(zip(range_s, range_m), desc = 's, m'):
+        if s not in step_error.keys():
+            step_error[s] = {}
+        step_error[s][m] = {}
+        for h in hs:
+            step_error[s][m][h] = float(error_sum_CFsplit(h, s, m, overline_xs_split, ys_split, maxc = 1, maxp = maxp, use_max = use_max))
     return step_error
 
 # We first compute the error of a single step
-#range_s = [2, 3]
-#range_m = [12, 20]
-#step_error_split = compute_step_error_split(hs, range_s, range_m, maxp = 50, total_error_list = total_error_list, total_time_list = total_time_list, use_max = True)
+range_ss = [2, 3]
+range_ms = [12, 20]
+#step_error_split = compute_step_error_split(hs, range_ss, range_ms, maxp = 50, use_max = True)
 # json save step_error
 #with open('results/step_error_CFMagnus_split.json', 'w') as f:
 #    json.dump(step_error_split, f)
@@ -384,7 +373,7 @@ def minimize_cost_CFMagnus_split(hs, s, m, total_time, total_error, step_error, 
         cost_exponentials[h] = total_time*m/h
         if trotter_exponentials: 
             cost_exponentials[h] *= 5**(s-1)
-        errors[h] = total_time*step_error[total_error][total_time][s][m][h]/h
+        errors[h] = total_time*step_error[s][m][h]/h
 
     min_cost = np.inf
     for h in hs:
@@ -399,12 +388,11 @@ def minimize_cost_CFMagnus_split(hs, s, m, total_time, total_error, step_error, 
 fig, ax = plt.subplots(2, 2, figsize = (10,10))
 
 total_error_list = [1e-3, 1e-7, 1e-11, 1e-15]
-colors = ['r', 'g', 'b', 'black']
+colors = ['r', 'g', 'orange', 'b', 'purple', 'black']
+colors_s = ['g', 'b']
 
 # Now we select is the total error
 for total_error, ax in zip(total_error_list, ax.flatten()):
-    range_s = [1,2,3,4]
-    range_m = [1,2,5,11]
     for (s, m, c) in zip(range_s, range_m, colors):
         min_costs = []
         min_costs_h = []
@@ -441,17 +429,15 @@ for total_error, ax in zip(total_error_list, ax.flatten()):
 
     ax.legend()
 
-    range_ss = [2, 3]
-    range_ms = [12, 20]
-    colors_s = ['g', 'b']
+
     # Now we select is the total error
     for (s, m, c) in zip(range_ss, range_ms, colors_s):
         min_costs = []
         min_costs_h = []
         for total_time in total_time_list: #todo: Change the step error and minimization function here.
-            min_cost_h, min_cost = minimize_cost_CFMagnus_split(hs, s, m, total_time, total_error, step_error = step_error_split, trotter_exponentials = True)
+            #min_cost_h, min_cost = minimize_cost_CFMagnus(hs, s, m, total_time, total_error, step_error = step_error_cf, trotter_exponentials = True)
             #min_cost_h, min_cost = minimize_cost_trotter(hs, s, total_time, total_error, step_error = step_error_trotter, trotter_exponentials = True)
-            #min_cost_h, min_cost = minimize_cost_CFMagnus_split(hs, s, m, total_time, total_error, step_error = step_error_split, trotter_exponentials = True)
+            min_cost_h, min_cost = minimize_cost_CFMagnus_split(hs, s, m, total_time, total_error, step_error = step_error_split, trotter_exponentials = True)
             min_costs.append(min_cost)
             min_costs_h.append(min_cost_h)
 
@@ -487,4 +473,4 @@ for total_error, ax in zip(total_error_list, ax.flatten()):
 
 #plt.show()
 # save figure #todo: change name here
-fig.savefig('commutator_free_magnus_error.pdf', bbox_inches='tight')
+fig.savefig('figures/commutator_free_magnus_error.pdf', bbox_inches='tight')
